72c0f7427c9476bc0e4a9add337e0a833bf853c8
ISParashkevov18 <ISParashkevov18@codingburgas.bg>
2022-12-05 12:18:04 +0200

    Finished project

---
+++ src/main/java/com/example/sringmvcconfiguration/SringMvcConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sringmvcconfiguration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SringMvcConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SringMvcConfigurationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sringmvcconfiguration/SringMvcConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sringmvcconfiguration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SringMvcConfigurationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
